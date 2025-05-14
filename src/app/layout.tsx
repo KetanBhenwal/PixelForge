759cff1b1738adb652286d9e52e1ae2e98147b54
Ketan Bhenwal <ketan.bhenwal@lrn.com>
2025-05-14 14:09:02 +0530

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Link from "next/link";
 import Image from "next/image";
@@ -238,6 +239,8 @@ export default function RootLayout({
             }
           `}
         </script>
+        <SpeedInsights />
+
       </body>
     </html>
   );
